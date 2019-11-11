4c8e6e5a036353f5ce6e476a1846e262b8c51a82
Kelly Jimenez <kellykjd@gmail.com>
2019-11-11 17:15:31 -0300

    Se agrega la barra de busqueda al AppBar

--- MercadoEsclavo/app/src/main/java/com/example/recyclermercadoabierto/view/MainActivity.java
+++ MercadoEsclavo/app/src/main/java/com/example/recyclermercadoabierto/view/MainActivity.java
@@ -1,24 +1,57 @@
 package com.example.recyclermercadoabierto.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.recyclermercadoabierto.R;
 import com.example.recyclermercadoabierto.model.Producto;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements FragmentListaProductos.ListenerDeFragment{
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.mainActivity_toolbar);
+
+        toolbar = findViewById(R.id.mainActivity_toolbar);
+
         setSupportActionBar(toolbar);
+
         pegarFragment(new FragmentListaProductos());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId){
+
+/*
+            case R.id.verCarrito:
+            Toast.makeText(MainActivity.this, "Pendiente por programar", Toast.LENGTH_LONG).show();
+            break;
+*/
+            case R.id.buscar:
+            Toast.makeText(MainActivity.this, "Pendiente por programar", Toast.LENGTH_LONG).show();
+            break;
+        }
+        return true;
     }
 
     private void pegarFragment(Fragment fragment){
